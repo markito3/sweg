c87b6ec1c215b9c9e47ccea0b35d22685ab69663
Mark Ito <markito3@gmail.com>
2022-01-21 17:36:54 -0500

    bring create_hist.cxx into registration with create_hist.C

--- root/create_hist.cxx
+++ root/create_hist.cxx
@@ -9,8 +9,8 @@ int main()
   Int_t bins = 50;
   Double_t xlow = 0.0, xhigh = 10.0;
   TH1F *h1 = new TH1F("example_name", "example_title", bins = 50, xlow = 0.0, xhigh = 10.0);
-  for (Int_t i = 0; i < 100; i++) {
-    data = float(i*i) / 1000.0;
+  for (Int_t i = 0; i < 10000; i++) {
+    data = float(i*i) / 10000000.0;
     h1->Fill(data);
   }
   gDirectory->ls();
