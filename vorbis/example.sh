6a0dbc2d7ae7be20d7dac2fb0225269e2d9ac8d0
Mark Ito <markito3@gmail.com>
2022-05-14 19:22:06 -0400

    add vorbis example

---
+++ vorbis/example.sh
@@ -0,0 +1,5 @@
+cp ~/Music/vinyl/jonathan_richman_and_the_modern_lovers/rock_n_roll_with_the_modern_lovers/01-Sound\ 01.ogg  .
+vorbiscomment -l 01-Sound\ 01.ogg
+cat c.txt
+vorbiscomment -w -c c.txt 01-Sound\ 01.ogg 
+vorbiscomment -l 01-Sound\ 01.ogg 
